516727f02c4c893df8f441a446de1057d74b862b
Logan Scanlon <scan3ls@gmail.com>
2020-04-08 22:20:53 +0000

    Ignore CTRL+D presses and refactor for betty style

--- header.h
+++ header.h
@@ -9,9 +9,11 @@
 #include <sys/wait.h>
 #include <signal.h>
 
+extern char **environ;
+
 void remove_new_line(char *str);
 char **seperate_line(char *line);
 char *find_command_path(char *command);
-void execute_command(char *pathname, char **arg_array);
+void execute_command(char *pathname, char **arg_array, char *line);
 
 #endif
--- main.c
+++ main.c
@@ -1,37 +1,51 @@
 #include "header.h"
 
+/**
+ * main - entry point for custom shell
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
 	char *line = NULL;
 	size_t len = 0;
-	ssize_t read;
-	char **argv;
-	char *path;
+	ssize_t rd;
+	char **argv = NULL;
+	char *path = NULL;
 
 	/* wait for input */
 	printf("$ ");
+	/* Ignore signals */
 	signal(SIGINT, SIG_IGN);
 
-	while ((read = getline(&line, &len, stdin)) != -1)
+	while (1)
 	{
+		rd = getline(&line, &len, stdin);
+		/* handles Ctrl+D presses */
+		if (rd == -1)
+			continue;
 		/* remove nl */
 		remove_new_line(line);
-
-		if (line != NULL && read > 1)
+		/* verify a command is present */
+		if (line != NULL && rd > 1)
 		{
 			/* seperate line into command and arguments */
 			argv = seperate_line(line);
 			/* find command assume base path of /bin/*/
 			path = find_command_path(argv[0]);
 			/* execute command */
-			execute_command(path, argv);
-			/* reset pathname */
-			free(path);
-			free(argv);
+			execute_command(path, argv, line);
 		}
+		/* reset pathname */
+		if (path != NULL)
+			free(path);
+		if (argv != NULL)
+			free(argv);
 		/* Back to start */
 		printf("$ ");
 	}
-
 	free(line);
+
+	return (0);
 }
