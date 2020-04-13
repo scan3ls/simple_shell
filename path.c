849ac030dc3b822c546592581dbe838e9d1099d2
BigObasheer <838@holbertonschool.com>
2020-04-13 23:08:30 +0000

    Fix Betty

--- path.c
+++ path.c
@@ -1,32 +1,35 @@
 #include "header.h"
+/**
+ *pathTok - split Path into Tokens
+ *Return: Tokens
+ */
 
 char **pathTok(void)
 {
-  char **tokens;
-  char *tok;
-  int i;
-  char *path;
-  char *env = *environ;
+	char **tokens;
+	char *tok;
+	int i;
+	char *path;
+	char *env = *environ;
 
+	for (i = 1; env != NULL; i++)
+	{
+		if (env[0] == 'P' && env[1] == 'A')
+			break;
+		env = *(environ + i);
+	}
+	path = env + 5;
+	i = 0;
 
-  for (i = 1; env != NULL; i++)
-  {
-	  if (env[0] == 'P' && env[1] == 'A')
-		  break;
-	  env = *(environ + i);
-  }
-  path = env + 5;
-  i = 0;
+	tokens = malloc(1000);
 
-  tokens = malloc(1000);
-
-  tok = strtok(path, ":");
-  while (tok != NULL)
-    {
-      tokens[i] = tok;
-      i++;
-      tok = strtok(NULL, ":");
-    }
-  tokens[i] = NULL;
-  return (tokens);
+	tok = strtok(path, ":");
+	while (tok != NULL)
+	{
+		tokens[i] = tok;
+		i++;
+		tok = strtok(NULL, ":");
+	}
+	tokens[i] = NULL;
+	return (tokens);
 }
