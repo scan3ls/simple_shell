057c8069b05f119a51307ab3767c4b32c9d35773
BigObasheer <838@holbertonschool.com>
2020-04-15 18:53:38 +0000

    Add Error Exit code

--- execute_command.c
+++ execute_command.c
@@ -9,13 +9,12 @@
 
 void execute_command(char **arg_array, char *line, char **tokens)
 {
-	int status;
+	int status = 0;
 	int pathCount, cmdLen, pathLen;
-	char *error[] = {"/bin/echo", "-n", "Error\n", NULL};
 	char *path;
 
 	/*checks for and executes built-ins */
-	builtIns(arg_array, line, tokens);
+	builtIns(arg_array, line, tokens, status);
 
 	if (fork() == 0)
 	{
@@ -38,9 +37,11 @@ void execute_command(char **arg_array, char *line, char **tokens)
 				execve(path, arg_array, NULL);
 				free(path);
 			}
+			status = 127;
 			free(arg_array);
 			/* print "error" using echo to close pid */
-			execve(error[0], error, NULL);
+			perror(arg_array[0]);
+			exit(status);
 		}
 	}
 	else
@@ -55,7 +56,7 @@ void execute_command(char **arg_array, char *line, char **tokens)
  *@tokens: array of directories in PATH
  */
 
-void builtIns(char **arg_array, char *line, char **tokens)
+void builtIns(char **arg_array, char *line, char **tokens, int status)
 {
 	int i;
 	char *env = *environ; /* grabs local enviorn with external variable */
@@ -68,7 +69,7 @@ void builtIns(char **arg_array, char *line, char **tokens)
 		free(arg_array);
 		free(line);
 		free(tokens);
-		exit(0);
+		exit(status);
 	}
 
 	/* check for env command */
