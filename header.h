2907ff0478de95b393246c8b1dfa256973431212
Logan Scanlon <scan3ls@gmail.com>
2020-04-16 03:21:25 +0000

    Correct PATH handling

--- header.h
+++ header.h
@@ -16,10 +16,11 @@ char **seperate_line(char *line);
 char *find_command_path(char *command);
 void execute_command(char **arg_array, char *line, char **tokens);
 char **pathTok(void);
-void builtIns(char **arg_array, char *line, char **tokens, int status);
+void builtIns(char **arg_array, char *line, char **tokens);
 void newPath(char *pth, char **tok, int pathL, int cmdL, int pathC, char *cmd);
 int getLength(char *str);
 int _strcmp(char *s1, char *s2);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char *_strdup(char *str);
 
 #endif
