a9d4321a616eebfb97284a23e2d3a7eb2f83b08e
Logan Scanlon <scan3ls@gmail.com>
2020-04-16 04:56:43 +0000

    Revert Merge pull request #22 from scan3ls/logan

--- header.h
+++ header.h
@@ -14,13 +14,12 @@ extern char **environ;
 int remove_new_line(char *str);
 char **seperate_line(char *line);
 char *find_command_path(char *command);
-void execute_command(char **arg_array, char *line, char **tokens);
+void execute_command(char **arg_array, char *line, char **tokens, int status);
 char **pathTok(void);
 void builtIns(char **arg_array, char *line, char **tokens);
 void newPath(char *pth, char **tok, int pathL, int cmdL, int pathC, char *cmd);
 int getLength(char *str);
 int _strcmp(char *s1, char *s2);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char *_strdup(char *str);
 
 #endif
